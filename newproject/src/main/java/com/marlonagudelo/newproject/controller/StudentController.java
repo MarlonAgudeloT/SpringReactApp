244b148e2e1353827b078f6a24e6ec66bd47ad5a
MarlonAgudelo <mgagudelot@gmail.com>
2023-01-27 09:46:25 +0000

    Connect Front with Back

--- newproject/src/main/java/com/marlonagudelo/newproject/controller/StudentController.java
+++ newproject/src/main/java/com/marlonagudelo/newproject/controller/StudentController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+
+@CrossOrigin
+
 @RequestMapping("/student")
 public class StudentController {
     @Autowired
